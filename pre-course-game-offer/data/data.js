dde55a0471a110b185e91d69260f20e77b85120a
Julyetta Karol <julietta_karol@mail.ru>
2024-04-22 23:57:45 +0300

    first commit

---
+++ pre-course-game-offer/data/data.js
@@ -0,0 +1,30 @@
+ export const data = {
+    settings:{
+        columns: 3,
+        rows: 3,
+        pointsWin:10,
+        deltaTimeInMs: 100,
+        maxMisses:3,
+        isMuted: false
+    },
+
+    scores:{
+        catchOffer: 1,
+        missOffer: 1
+    },
+
+    coords:{
+        current:{
+            x: 2,
+            y: 0
+        },
+        catch:{
+            x: 0,
+            y: 0
+        },
+        missed:{
+            x: 1,
+            y: 1
+        },
+    }
+ }
